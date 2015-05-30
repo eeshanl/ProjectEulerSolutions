68ce9267fc59c665dd6a369b479c6adc57390ad9
eeshanl <eeshan@uw.edu>
2015-05-30 16:53:43 -0700

    edits

--- src/ProjectEulerMain.java
+++ src/ProjectEulerMain.java
@@ -8,10 +8,10 @@ public class ProjectEulerMain {
 		Problems problems = new Problems();
 		// List storing all solutions
 		ArrayList<Integer> solutionList = new ArrayList<Integer>();
-		
-		// Problem number 1 added 
+
+		// Problem number 1 added
 		solutionList.add(problems.One_Sum3and5(1000));
-		
+
 		// Prints out solutions to problems in an array format
 		System.out.println(solutionList);
 	}
