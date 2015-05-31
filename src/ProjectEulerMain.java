26fea97cde6f88c75741739d8e4d892509d217d4
eeshanl <eeshan@uw.edu>
2015-05-30 17:08:18 -0700

    edits

--- src/ProjectEulerMain.java
+++ src/ProjectEulerMain.java
@@ -3,17 +3,23 @@ import java.util.ArrayList;
 public class ProjectEulerMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Class containing all problem solutions to Project Euler Problems
+		// Problems class containing all problem solutions to Project Euler Problems
 		Problems problems = new Problems();
 		// List storing all solutions
 		ArrayList<Integer> solutionList = new ArrayList<Integer>();
 
 		// Problem number 1 added
-		solutionList.add(problems.One_Sum3and5(1000));
+		solutionList.add(problems.One(1000));
+		solutionList.add(problems.Two());
 
 		// Prints out solutions to problems in an array format
-		System.out.println(solutionList);
+		printSoln(solutionList);
+	}
+	
+	public static void printSoln(ArrayList<Integer> soln) {
+		for (int i = 0; i < soln.size(); i++) {
+			System.out.println("The solution to problem number " + (i+1) + " is " + soln.get(i) + ".");
+		}
 	}
 
 }
